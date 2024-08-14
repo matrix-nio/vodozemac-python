296de56e1eafc58513371c88749d25e414e83371
Damir Jelić <poljar@termina.org.uk>
2024-08-14 17:23:36 +0200

    Add stronger typing for the Megolm session keys

---
+++ src/types/session_keys.rs
@@ -0,0 +1,52 @@
+use crate::error::*;
+use pyo3::prelude::*;
+
+#[pyclass]
+pub struct SessionKey {
+    pub(crate) inner: vodozemac::megolm::SessionKey,
+}
+
+#[pymethods]
+impl SessionKey {
+    #[new]
+    pub fn from_base64(session_key: &str) -> Result<Self, SessionKeyDecodeError> {
+        Ok(Self {
+            inner: vodozemac::megolm::SessionKey::from_base64(session_key)?,
+        })
+    }
+
+    pub fn to_base64(&self) -> String {
+        self.inner.to_base64()
+    }
+}
+
+impl From<vodozemac::megolm::SessionKey> for SessionKey {
+    fn from(value: vodozemac::megolm::SessionKey) -> Self {
+        Self { inner: value }
+    }
+}
+
+#[pyclass]
+pub struct ExportedSessionKey {
+    pub(crate) inner: vodozemac::megolm::ExportedSessionKey,
+}
+
+#[pymethods]
+impl ExportedSessionKey {
+    #[new]
+    pub fn from_base64(session_key: &str) -> Result<Self, SessionKeyDecodeError> {
+        Ok(Self {
+            inner: vodozemac::megolm::ExportedSessionKey::from_base64(session_key)?,
+        })
+    }
+
+    pub fn to_base64(&self) -> String {
+        self.inner.to_base64()
+    }
+}
+
+impl From<vodozemac::megolm::ExportedSessionKey> for ExportedSessionKey {
+    fn from(value: vodozemac::megolm::ExportedSessionKey) -> Self {
+        Self { inner: value }
+    }
+}
