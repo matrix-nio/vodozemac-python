288157efeaf16e812fa477df923a9fe1fdb2b863
Paarth Shah <mail@shahpaarth.com>
2025-06-30 01:47:21 -0700

    Add python3.13, use `uv` for dev, bump dependencies (#17)
    * Bump maturin to 1.9.0
    
    * Use uv for dev/test/nox
    
    * Update python workflow to use uv
    
    * Update release workflow
    
    * Cargo update + bump pyo3 to 0.25.1
    
    * Remove broken emscripten

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,11 @@
-import nox
+import nox_uv
+from nox import Session, options
 
+options.default_venv_backend = "uv"
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
-def test(session):
-    session.install("-rrequirements-dev.txt")
-    session.install("-e", ".", "--no-build-isolation")
+@nox_uv.session(
+    python=["3.9", "3.10", "3.11", "3.12", "3.13"],
+    uv_groups=["tests"],
+)
+def test(session: Session):
     session.run("pytest")
